ec406d3d74f54bb9aeaea7ad7c5588dba7d02021
7gtz <71064991+7gtz@users.noreply.github.com>
2025-09-11 00:37:32 +0530

    Changes to be committed: 	modified:   src/App.jsx
    Changes not staged for commit:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Leaderboard from "./pages/Leaderboard";
 import HomePage from "./pages/Home.jsx";
 
+
 function App() {
   return (
     <Router>
